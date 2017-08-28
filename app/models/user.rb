1250994de2cdbb36dfdb0f2b97ac5cd7ee5d4163
Nana Adjei Manu <claeusdev@gmail.com>
2017-08-28 08:53:08 +0000

    Change placeholder default user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 	include Concerns::Following
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   devise :database_authenticatable, :registerable,
